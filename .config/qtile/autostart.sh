0d8b6375a2464582ce710820c7c2a50b0bc1d6ec
Stefan Ekblom <Stefan.Ekblom@Dental-IT.se>
2024-02-27 09:44:32 +0100

    changed so that a random wallpaper loads at start

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -13,7 +13,7 @@ conky -c "$HOME"/.config/conky/qtile/01/"$COLORSCHEME".conf || echo "Couldn't st
 
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
 # 1. Uncomment to restore last saved wallpaper
-xargs xwallpaper --stretch < ~/.cache/wall &
+xwallpaper --stretch "$(ls $HOME/Pictures/wallpapers | shuf -n 1)"  &
 # 2. Uncomment to set a random wallpaper on login
 # find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
 # 3. Uncomment to set wallpaper with nitrogen
