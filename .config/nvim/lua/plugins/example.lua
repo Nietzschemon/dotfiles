ecdd9943721f8b002c90ad7a54bf1bcb4d8ad038
Stefan Ekblom <Stefan.Ekblom@Dental-IT.se>
2024-03-14 17:25:59 +0100

    fixed copilot implemtation

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -214,6 +214,10 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-emoji",
+            {
+                "zbirenbaum/copilot-cmp",
+                opts = {},
+            },
         },
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
@@ -226,6 +230,7 @@ return {
 
             local luasnip = require("luasnip")
             local cmp = require("cmp")
+            opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, name = "emoji" }))
             opts.completion.completeopt = "menu,menuone,noselect"
             opts.mapping = vim.tbl_extend("force", opts.mapping, {
                 ["<Tab>"] = cmp.mapping(function(fallback)
